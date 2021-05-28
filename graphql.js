ed09e5eb2839f59130a08caf87e7daeca6c757bb
matthew-js-porter <matthew.js.porter@jpmchase.com>
2021-05-28 16:46:27 -0400

    adding starter project for graphql running on aws using serverless.
    adding a basic hello world project that hosts a graphql server both locally and as a servless app using aws and the serverless framework.

---
+++ graphql.js
@@ -0,0 +1,15 @@
+const { ApolloServer } = require('apollo-server-lambda');
+const { readFileSync } = require('fs');
+const resolvers = require('./resolvers');
+const typeDefs = readFileSync('./schema.graphql').toString('utf-8')
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+
+    playground: {
+        endpoint: "/dev/graphql"
+    }
+});
+
+exports.graphqlHandler = server.createHandler();
---
+++ server.js
@@ -0,0 +1,18 @@
+const { ApolloServer } = require('apollo-server');
+const { readFileSync } = require('fs');
+const resolvers = require('./resolvers');
+const typeDefs = readFileSync('./schema.graphql').toString('utf-8')
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+
+    playground: {
+        endpoint: "/dev/graphql"
+    }
+});
+
+server.listen(4000, ()=>{
+    console.log('server started on port 4000');
+    console.log('http://localhost:4000')
+});
