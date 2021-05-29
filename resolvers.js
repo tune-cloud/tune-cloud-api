26964a19cabb10056fe8b688e85747d30945bb8e
matthew-js-porter <matthew.js.porter@jpmchase.com>
2021-05-29 08:16:42 -0400

    removing hello query and adding artist query for finding song artists

--- resolvers.js
+++ resolvers.js
@@ -1,6 +1,17 @@
+const Genius = require("genius-lyrics");
+const Client = new Genius.Client();
+const ArtistService = require('./artists/artist-service');
+
+const artistService = new ArtistService(Client);
+
 const resolvers = {
     Query: {
-        hello: () => 'Hello world!',
+        artists(parent, args, context, info) {
+            return artistService.find(args.search).then((result) => {
+                console.log(result);
+                return result;
+            });
+        },
     },
 };
 
