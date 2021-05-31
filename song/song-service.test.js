e467b201e72c40083dbef230c80ae943ccc31e2d
matthew-js-porter <matthew.js.porter@jpmchase.com>
2021-05-31 00:41:56 -0400

    refactoring song-service.test.js to remove duplicate code.

--- song/song-service.test.js
+++ song/song-service.test.js
@@ -8,16 +8,21 @@ const expect = chai.expect;
 chai.use(chaiAsPromised);
 
 describe('SongService', ()=> {
+  let artist;
+  let songService;
+
+  beforeEach(()=>{
+    const Client = sinon.mock(new Genius.Client());
+    artist = new Genius.Artist({}, '1234', null, {});
+
+    const artistClient = new Genius.ArtistsClient();
+    sinon.stub(artistClient, 'get').returns(artist);
+    Client.artists = artistClient;
+
+    songService = new SongService(Client);
+  });
+
   describe('_fetchPage', ()=> {
-    let artist;
-    let songService;
-
-    beforeEach(()=>{
-      const Client = sinon.mock(new Genius.Client());
-      artist = new Genius.Artist({}, '1234', null, {});
-      songService = new SongService(Client);
-    });
-
     it('should fetch a page of songs', async ()=> {
       const expectedSongs = [{
         id: 'id',
@@ -68,13 +73,6 @@ describe('SongService', ()=> {
 
   describe('getSongs', ()=> {
     it('should get songs', async ()=>{
-      const Client = sinon.mock(new Genius.Client());
-      const artistClient = new Genius.ArtistsClient();
-      Client.artists = artistClient;
-
-      const artist = new Genius.Artist({}, '1234', null, {});
-      sinon.stub(artistClient, 'get').returns(artist);
-
       const expectedSongs = [{
         id: 'id',
         title: 'title',
@@ -86,7 +84,6 @@ describe('SongService', ()=> {
       sinon.stub(artist, 'songs').onCall(0).returns(expectedSongs)
           .onCall(1).throws(new Error(Genius.Constants.NO_RESULT));
 
-      const songService = new SongService(Client);
       const songs = await songService.getSongs('1234');
 
       expect(songs).to.deep.equal(expectedSongs);
